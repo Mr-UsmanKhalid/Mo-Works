35b9e73479c8aa7fa086c141cb12a9411c71ec21
HamzaShahid59 <hs623965@gmail.com>
2026-06-16 14:57:51 +0500

    language added for about section

--- components/AboutSection.jsx
+++ components/AboutSection.jsx
@@ -4,18 +4,19 @@ import { Badge, Box, Chip, Container, Grid, Typography, useTheme } from '@mui/ma
 import React from 'react'
 import { motion } from 'framer-motion';
 import { Calendar, Clock, Code, Users } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 const AboutSection = () => {
-
+    const t = useTranslations("About");
     const theme = useTheme();
 
-   
+
     return (
         <>
             <section id="about" className="py-24 bg-white">
                 <Container maxWidth='lg'>
                     <Box className="mb-16 text-center">
-                        <Chip variant='outlined' label="About Us" className="mb-3" />
+                        <Chip variant="outlined" label={t("label")} className="mb-3" />
                         <Typography
                             variant="h2"
                             component="h1"
@@ -27,7 +28,7 @@ const AboutSection = () => {
                                 lineHeight: 1.2
                             }}
                         >
-                            Who we are
+                            {t("title")}
                         </Typography>
 
                         <Typography variant='body1' component='p'
@@ -38,18 +39,7 @@ const AboutSection = () => {
                                 mx: 'auto'
                             }}
                         >
-                           We are a boutique IT consultancy specializing in Agile transformation, Data Engineering, and AI solutions. Our mission is to help organizations become faster, smarter, and more adaptable in an ever-evolving digital world. 
-
-We believe that technology should empower people to make better decisions, deliver faster, and innovate with confidence. By combining Agile principles, modern data architectures, and responsible AI practices, we enable our clients to turn complexity into clarity  and insight into impact. 
-
-From redefining delivery models to building intelligent data pipelines and deploying AI-powered applications, we partner closely with teams to design solutions that create measurable business value. 
-
-Agility. Data. AI. Transformation. 
-It’s not just what we do  it’s how we think. 
-
-Shape 
-
-Perfect  here’s your refined “Agile Transformation Services” section with the added emphasis on your certified trainers and practitioners under Why Partner With Us. 
+                            {t("description")}
                         </Typography>
 
                     </Box>
